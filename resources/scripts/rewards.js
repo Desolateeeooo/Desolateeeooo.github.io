d9114c71a7d1a8fa2ab0bfccf1af58872b12a075
Desolateeeooo <125212823+Desolateeeooo@users.noreply.github.com>
2023-12-07 19:55:26 +0500

    rewards.js: add clearAll and local storage
    added functionality for ClearAll button.
    added local storage for rewards

--- resources/scripts/rewards.js
+++ resources/scripts/rewards.js
@@ -1,7 +1,37 @@
+const truncate = (str, maxlength) => {
+	return (str.length > maxlength) ?
+	str.slice(0, maxlength - 1) + '…' : str;
+}
+
+const remove = (e) => {
+	const el = e.target;
+	el.parentNode.remove();
+	// localButtonsArr
+}
+
+const createButtons = () => {
+	let rewardStorage = localStorage.getItem('rewardButtons');
+	const rewardButtons = JSON.parse(rewardStorage ?? '[]');
+	rewardButtons.forEach(reward => {
+		const li = document.createElement("li");
+		let rewardBtn = document.createElement("input");
+    rewardBtn.type = "button";
+    rewardBtn.value = `${truncate(reward._name, 35)} for ${parseInt(reward._cost)} points`;
+    rewardBtn.onclick = remove;
+		rewardBtn.classList.add('addedReward');
+    li.appendChild(rewardBtn);
+    document.querySelector("#list").appendChild(li);
+	});
+	return rewardButtons;
+}
+
+let localButtonsArr = createButtons();
+
+const list = document.querySelector("#list");
 const pushRewards = () => () => {
 	const i1 = document.querySelector("#rewardName").value;
 	const i2 = document.querySelector("#cost").value;
-
+	
 	const li = document.createElement("li");
 
 	let rewardBtn = document.createElement("input");
@@ -10,21 +40,19 @@ const pushRewards = () => () => {
     rewardBtn.onclick = remove;
 		rewardBtn.classList.add('addedReward');
     li.appendChild(rewardBtn);
-    document.querySelector("#list").appendChild(li);
-}
-
-const remove = (e) => {
-	const el = e.target;
-	el.parentNode.remove();
-}
-
-const truncate = (str, maxlength) => {
-	return (str.length > maxlength) ?
-	str.slice(0, maxlength - 1) + '…' : str;
+    list.appendChild(li);
+		const buttonProperties = {_name: i1, _cost: i2};
+		localButtonsArr.push(buttonProperties);
+		localStorage.setItem('rewardButtons', JSON.stringify(localButtonsArr));
 }
 
 const addRewardButton = document.querySelector("#reward");
-const clearButton = document.querySelector("#clear");
-
 addRewardButton.addEventListener("click", pushRewards());
-// clearButton.addEventListener("click", remove());
+
+const clearButton = document.querySelector("#clear");
+const clearAll = () => {
+	localStorage.removeItem("rewardButtons");
+	localButtonsArr = [];
+	list.innerHTML = '';
+}
+clearButton.addEventListener("click", clearAll);
